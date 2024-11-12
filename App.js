0d28f51204da7d91b6405590d008acfd49fb8c09
reshab/181 <reshabkumar99@gmail.com>
2024-11-12 14:14:51 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const Parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am h1 tag"),
-    React.createElement("h2", {}, "I am h2 tag"),
+    React.createElement("h1", {}, "This is Namste React"),
+    React.createElement("h2", {}, "tytfyediuehdoiewioe"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I am h1 tag"),
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Namaste React
+
+# Parcel
+
+- Dev build
+- Local Server
+- Hot Reload (HMR) Hot Module Reloading
+- File Watching Algorithm- written in C++
+- Caching- Faster Builds
+- Image Optimization
+- Code Minification
+- Bundling
+- Compressing
+- Lazy Loading
+- Consistent Hashing
+- Code Splitting
+- Tree Shaking- remove unused code
+- Differtial Bundling - support older Browser
+- Error Handling
+- Diagnostic
+- Https
+- Different dev and Prod bundles
